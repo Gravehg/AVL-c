050566b0da0eecea5970970b17596c0e92098724
Gravehg <80481681+Gravehg@users.noreply.github.com>
2024-07-14 13:00:25 -0600

    Implemented remove, remove aux, is leaf, unique child and children count functions

--- AVL.c
+++ AVL.c
@@ -13,6 +13,7 @@ AVL *get_avl(){
 
 void insert_avl(AVL *avl, int element){
     avl->root = insert_avl_aux(avl->root, element);
+    avl->size++;
 }
 
 AVLNode *insert_avl_aux(AVLNode *root, int element){
@@ -36,4 +37,56 @@ AVLNode *get_succesor_avl(AVLNode *root){
     }else{
         return get_succesor_avl(root->left);
     }
+}
+
+int remove_avl(AVL *avl, int element){
+    int result;
+    avl->root = remove_avl_aux(avl->root, element, &result);
+    avl->size--;
+    return result;
+}
+
+AVLNode *remove_avl_aux(AVLNode *root, int element, int *result){
+    if(root == NULL){
+        printf("%s\n", "Can't remove element that is not in the AVL data structure");
+        exit(EXIT_FAILURE);
+    }
+    if(element < root->element){
+        root->left = remove_avl_aux(root->left, element, result);
+        return rebalance_right(root);
+    }else if(element > root->element){
+        root->right = remove_avl_aux(root->right, element, result);
+        return rebalance_left(root);
+    }else{
+        *result = root->element;
+        if(is_leaf_avl(root)){
+            free(root);
+            return NULL;
+        }
+        if(node_children_count(root) == 1){
+            AVLNode *child = get_unique_child_avl(root);
+            free(root);
+            return child;
+        }else{
+            AVLNode *succ = get_succesor_avl(root->right);
+            int temp = succ->element;
+            succ->element = root->element;
+            root->element = temp;
+            root->right = remove_avl_aux(root->right, element, result);
+            return rebalance_left(root);
+        }
+    }
+}
+
+int is_leaf_avl(AVLNode *root){
+    return root->left == NULL && root->right == NULL;
+}
+
+int node_children_count(AVLNode *root){
+    return (root->left == NULL ? 0 : 1) + (root->right == NULL ? 0
+                                                            : 1);
+}
+
+AVLNode *get_unique_child_avl(AVLNode *root){
+    return root->left == NULL ? root->right : root->left;
 }
